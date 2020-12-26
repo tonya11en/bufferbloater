4412b386eefef012d9334207d60c95c28311ee78
Tony Allen <tony@allen.gg>
2020-12-26 12:51:51 -0800

    plots
    Signed-off-by: Tony Allen <tony@allen.gg>

--- generate_report.py
+++ generate_report.py
@@ -33,69 +33,106 @@ def xy_from_csv(filename):
 # We want to plot the request are, latency, and the moment timeouts happen.
 # While we're at it, let's just adjust the timestamp to be relative to the
 # simulation start.
-rq_rate_x, rq_rate_y = xy_from_csv("client.rps.csv")
-rq_latency_x, rq_latency_y = xy_from_csv("client.rq.latency.csv")
-rq_sr_x, rq_sr_y = xy_from_csv("client.rq.success_rate.csv")
-qsize_x, qsize_y = xy_from_csv("server.queue.size.csv")
-timeout_stamps, _ = xy_from_csv("client.rq.timeout.csv")
-service_unavail_stamps, _ = xy_from_csv("client.rq.503.csv")
+rq_rate_x1, rq_rate_y1 = xy_from_csv("client1.rps.csv")
+rq_rate_x2, rq_rate_y2 = xy_from_csv("client2.rps.csv")
 
-sim_start = min(rq_rate_x + rq_latency_x + timeout_stamps)
-rq_rate_x = list(map(lambda x: (x - sim_start) / 1e9, rq_rate_x))
-rq_latency_x = list(map(lambda x: (x - sim_start) / 1e9, rq_latency_x))
-rq_sr_x = list(map(lambda x: (x - sim_start) / 1e9, rq_sr_x))
-qsize_x = list(map(lambda x: (x - sim_start) / 1e9, qsize_x))
-timeout_stamps = list(map(lambda x: (x - sim_start) / 1e9, timeout_stamps))
-service_unavail_stamps = list(map(lambda x: (x - sim_start) / 1e9, service_unavail_stamps))
+rq_latency_x1, rq_latency_y1 = xy_from_csv("client1.rq.latency.csv")
+rq_latency_x2, rq_latency_y2 = xy_from_csv("client2.rq.latency.csv")
 
-relative_sim_end = max(rq_latency_x + timeout_stamps + service_unavail_stamps)
-rq_sr_x = list(map(lambda x: x * (1.0 * relative_sim_end / rq_sr_x[-1]), rq_sr_x))
-qsize_x = list(map(lambda x: x * (1.0 * relative_sim_end / qsize_x[-1]), qsize_x))
+rq_sr_x1, rq_sr_y1 = xy_from_csv("client1.rq.success_rate.csv")
+rq_sr_x2, rq_sr_y2 = xy_from_csv("client2.rq.success_rate.csv")
+
+active_rq_x, active_rq_y = xy_from_csv("server.active_rq.csv")
+
+qsize_x, qsize_y = xy_from_csv("server.queued_rq.csv")
+
+qtimeout_x, qtimeout_y = xy_from_csv("server.queued_rq.csv")
+
+timeout_stamps1, _ = xy_from_csv("client1.rq.timeout.csv")
+timeout_stamps2, _ = xy_from_csv("client2.rq.timeout.csv")
+
+service_unavail_stamps1, _ = xy_from_csv("client1.rq.503.csv")
+service_unavail_stamps2, _ = xy_from_csv("client2.rq.503.csv")
+
+sim_start = min(rq_rate_x1 + rq_latency_x1 + timeout_stamps1 + rq_rate_x2 + rq_latency_x2 + timeout_stamps1 + timeout_stamps2)
+
+# Normalize start times for x-vals.
+def adjust_x_val_starts(vals):
+    return list(map(lambda x: (x - sim_start) / 1e9, vals))
+
+rq_rate_x1 = adjust_x_val_starts(rq_rate_x1)
+rq_rate_x2 = adjust_x_val_starts(rq_rate_x2)
+rq_latency_x1 = adjust_x_val_starts(rq_latency_x1)
+rq_latency_x2 = adjust_x_val_starts(rq_latency_x2)
+qtimeout_x = adjust_x_val_starts(qtimeout_x)
+timeout_stamps1 = adjust_x_val_starts(timeout_stamps1)
+timeout_stamps2 = adjust_x_val_starts(timeout_stamps2)
+service_unavail_stamps1 = adjust_x_val_starts(service_unavail_stamps1)
+service_unavail_stamps2 = adjust_x_val_starts(service_unavail_stamps2)
+rq_sr_x1 = adjust_x_val_starts(rq_sr_x1)
+rq_sr_x2 = adjust_x_val_starts(rq_sr_x2)
+active_rq_x = adjust_x_val_starts(active_rq_x)
+qsize_x = adjust_x_val_starts(qsize_x)
+
+relative_sim_end = max(rq_rate_x1 + rq_rate_x2 +
+                       rq_latency_x1 + rq_latency_x2 +
+                       qtimeout_x + timeout_stamps1 + timeout_stamps2 +
+                       service_unavail_stamps1 + service_unavail_stamps2 + 
+                       rq_sr_x1 + rq_sr_x2 + active_rq_x + qsize_x)
+
+def adjust_x_val_ends(vals):
+    return list(map(lambda x: x * (1.0 * relative_sim_end / vals[-1]), vals))
+
+rq_sr_x1 = adjust_x_val_ends(rq_sr_x1)
+rq_sr_x2 = adjust_x_val_ends(rq_sr_x2)
+active_rq_x = adjust_x_val_ends(active_rq_x)
+qsize_x = adjust_x_val_ends(qsize_x)
 
 fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6)
 
+print(rq_latency_x1)
 ax1.set_xlabel('Time (s)')
 ax1.set_xlim([0,relative_sim_end])
 ax1.set_ylabel('Request Latency')
-ax1.scatter(rq_latency_x,rq_latency_y)
+ax1.scatter(rq_latency_x1,rq_latency_y1)
+ax1.scatter(rq_latency_x2,rq_latency_y2)
 ax1.tick_params(axis='y', labelcolor="black")
 
 ax2.set_xlabel('Time (s)')
 ax2.set_xlim([0,relative_sim_end])
 ax2.set_ylabel('RPS')
-ax2.plot(rq_rate_x,rq_rate_y, '--')
+ax2.plot(rq_rate_x1,rq_rate_y1, '--')
+ax2.plot(rq_rate_x2,rq_rate_y2, '--')
 ax2.tick_params(axis='y', labelcolor="blue")
 
 # Get timeout vertical lines.
 ax3.set_ylabel("Request Timeouts")
 ax3.set_xlabel('Time (s)')
 ax3.set_xlim([0,relative_sim_end])
-if len(timeout_stamps) > 0:
+if len(timeout_stamps1 + timeout_stamps2) > 0:
     bins = np.arange(0, relative_sim_end, 0.01)
-    hist, _ = np.histogram(timeout_stamps, bins)
-    extent = [bins.min(), bins.max(), 0, 1]
-    ax3.hist(timeout_stamps, bins=1000, range=(0,relative_sim_end), color="red")
+    ax3.hist([timeout_stamps1, timeout_stamps2], bins=1000, density=True, histtype='bar', stacked=True, range=(0,relative_sim_end))
 
 # Get 503 vertical lines.
 ax4.set_ylabel("Request 503s")
 ax4.set_xlabel('Time (s)')
 ax4.set_xlim([0,relative_sim_end])
-if len(service_unavail_stamps) > 0:
+if len(service_unavail_stamps1 + service_unavail_stamps2) > 0:
     bins = np.arange(0, relative_sim_end, 0.01)
-    hist, _ = np.histogram(service_unavail_stamps, bins)
-    extent = [bins.min(), bins.max(), 0, 1]
-    ax4.hist(service_unavail_stamps, bins=1000, range=(0,relative_sim_end), color="green")
+    ax4.hist([service_unavail_stamps1,service_unavail_stamps2], bins=1000, density=True, histtype='bar',
+            stacked=True, range=(0,relative_sim_end), color=["green", "blue"])
 
 ax5.set_xlabel('Time (s)')
 ax5.set_xlim([0,relative_sim_end])
 ax5.set_ylabel('Service Queue Size')
-ax5.plot(qsize_x,qsize_y, '-')
+ax5.plot(qsize_x, qsize_y, '-')
 ax5.tick_params(axis='y', labelcolor="blue")
 
 ax6.set_xlabel('Time (s)')
 ax6.set_xlim([0,relative_sim_end])
 ax6.set_ylabel('Request Success Rate')
-ax6.plot(rq_sr_x, rq_sr_y, '-')
+ax6.plot(rq_sr_x1, rq_sr_y1, '-')
+ax6.plot(rq_sr_x2, rq_sr_y2, '-')
 ax6.tick_params(axis='y', labelcolor="blue")
 
 plt.legend()
